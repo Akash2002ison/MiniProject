e381797ca38e0af7d77ba5d0cd12062b5322162a
Akash Tiwari <156169466+akash-tiwari-amla@users.noreply.github.com>
2024-04-03 13:53:22 +0530

    MiniProject
    Folder Structure

---
+++ QuizWebAPIModel/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWebAPIModel
+{
+    public class Class1
+    {
+    }
+}
